6aba472ff8293de89c68d5f904066d4c03077fcc
Rahul Kumar Gaur <155457116+Rahulindian@users.noreply.github.com>
2026-04-13 20:38:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.66.0"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mango" {
+  name     = "mango_alphanso"
+  location = "westus"
+}
